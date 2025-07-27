537a40ee7bb4901156ba3c2d379c9b5b3285b130
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-27 17:47:43 +0600

    Variable naming

---
+++ Module 3 Hello JavaScript/Introduction to JavaScript/naming.js
@@ -0,0 +1,14 @@
+/* Variable Naming
+1. No keywords in variable name
+2. No space or gap in variable name
+3. No quote in variable name
+4. Can not start with a number as first latter
+5. Name in case sensative
+*/
+
+// 6. Variable name writting 
+var my current home address = 'Munshiganj Dhaka'; // not valid
+var mycurrenthomeaddress = 'Munshiganj Dhaka'; // not readable 
+var my_current_home_address = 'Munshiganj Dhaka'; //Snake Case
+var myCurrentHomeAddress = 'Munshiganj Dhaka';  // Camel Case
+var MyCurrentHomeAddress = 'Munshiganj Dhaka';  // Pascal Case
