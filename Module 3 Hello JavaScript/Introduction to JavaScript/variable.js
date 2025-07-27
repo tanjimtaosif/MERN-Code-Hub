e25017a201b654b816718b1af6b773c588de2046
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-27 17:47:30 +0600

    Learn about Variables

--- Module 3 Hello JavaScript/Introduction to JavaScript/variable.js
+++ Module 3 Hello JavaScript/Introduction to JavaScript/variable.js
@@ -1,4 +1,12 @@
 /* 
 1. Variable ==> Vary + Able
-2. 
+2. 5 things to declare a variable 
 */
+
+var weight = 38;
+var price = 38;
+var year = 1995;
+var money = 2003;
+
+console.log(money);
+console.log(price);
