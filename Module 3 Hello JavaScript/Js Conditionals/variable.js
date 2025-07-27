c11aba230bad86294049a87c2929c81d434ec0a3
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-28 01:15:37 +0600

    Recap variables and started learning conditional

---
+++ Module 3 Hello JavaScript/Js Conditionals/variable.js
@@ -0,0 +1,23 @@
+/* 
+Variable => Container
+*/
+
+// Number type
+var sunglassPrice = 80;
+
+// String
+var subject = 'CSE';
+
+// Boolean
+var passed = true;
+
+/*
+Naming Convention
+
+1. Signle Word
+2. no quote
+3. no gap or dash
+4. can not start with number
+5. can not be a keyword
+6. prefer camelCase
+*/
