e8940e739bd75bdc340d1fee37e02556a77a11a2
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-27 18:43:53 +0600

    Seen some short hand arithmatic  operstions

---
+++ Module 3 Hello JavaScript/Introduction to JavaScript/shorthand.js
@@ -0,0 +1,15 @@
+var price = 30;
+// price = price + 10; 
+price += 10;
+console.log(price);
+
+
+var fridge = 700;
+// fridge = fridge - 400;
+fridge -= 400;
+console.log(fridge);
+
+var red = 700;
+// red = red / 400;
+red -= 400;
+console.log(red);
