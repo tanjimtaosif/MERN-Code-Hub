31c0165bfa6c55f843b571cdf29def5d6ad4852e
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-27 17:47:58 +0600

    Data types

---
+++ Module 3 Hello JavaScript/Introduction to JavaScript/types.js
@@ -0,0 +1,23 @@
+// String
+var name = "Sadar Uddin";
+console.log(name);
+
+var address = "Andar killa bandarban"
+console.log(address);
+
+var country = 'Bangladesh'
+console.log(address);
+
+// Boolean
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+
+// Numeric
+var price = 120;
+console.log(price);
+
+
