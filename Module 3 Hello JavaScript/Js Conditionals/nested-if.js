6075d3dccb4860faaf0ad8b94c7f6ffdb41025a6
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-29 23:03:15 +0600

    Nested if else

---
+++ Module 3 Hello JavaScript/Js Conditionals/nested-if.js
@@ -0,0 +1,19 @@
+const money = 0;
+
+if (money > 300) {
+    console.log('Bro you are rich');
+}
+
+else {
+    if (money > 100) {
+        console.log("You are financially poor");
+    }
+    else {
+        if (money > 0) {
+            console.log("You have to earn money");
+        }
+        else {
+            console.log("You can not survive");
+        }
+    }
+}
