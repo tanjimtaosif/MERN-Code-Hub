8f67f947ed62e2979f590ba891e524b03f7530e1
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-29 22:05:11 +0600

    Practiced if else statement

---
+++ Module 3 Hello JavaScript/Js Conditionals/if-elseif-else.js
@@ -0,0 +1,21 @@
+/*
+Multi Level Condition
+*/
+
+const price = 5000;
+
+if (price >= 5000) {
+    const discount = price * 10 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+else if (price > 2500) {
+    const discount = price * 5 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount);
+}
+
+else {
+    console.log(price);
+
+}
