294b232ad334c63cee387d3b6beccf841e3144db
tanjimtaosif <itsmahtab007@gmail.com>
2025-07-29 21:23:46 +0600

    Learend about comparisons in JavaScript

---
+++ Module 3 Hello JavaScript/Js Conditionals/comparison.js
@@ -0,0 +1,36 @@
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15 >= 10);
+console.log(5 >= 10);
+console.log(5 <= 10);
+console.log(10 <= 10);
+console.log(100 <= 10);
+console.log(10 == 10.1);
+
+// ----------------------
+
+var orange = 120;
+var apple = 230;
+
+console.log(orange > apple);
+console.log(apple < orange);
+
+// -----------------
+
+console.log(10 == '10');
+console.log(10 === '10');
+
+console.log(120 === 'tomato');
+
+// --------------------
+
+console.log(10 != '10');
+console.log(10 !== '10');
+
+
+
+
+
