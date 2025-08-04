39c9a260a404664d489533b4810990c45aa29619
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-04 17:04:39 +0600

    All Code Togather

--- Assignment_3 - Javascipt Simple Problems/problem_1.js
+++
@@ -1,9 +0,0 @@
-/** Problem -01 ( Divide the Asset ) */
-var area = 800;
-
-// Write your code here
-var eachPersonGets = area / 2;
-
-// Show the result
-console.log(eachPersonGets);
-
--- Assignment_3 - Javascipt Simple Problems/problem_6.js
+++
@@ -1,7 +0,0 @@
-/** Problem 06 :  (Current Salary )  */
-var experience = 30;
-var startingSalary = 45000;
-
-// write your code here
-var currentSalary = startingSalary * Math.pow(1.05, experience);
-console.log(currentSalary.toFixed(2));
