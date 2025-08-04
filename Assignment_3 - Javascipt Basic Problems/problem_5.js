09f384afd0c9e2a7f1ab17ba0c635bafdbc1c1fc
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-04 17:03:57 +0600

    Problem 05 - PH Email Generator

---
+++ Assignment_3 - Javascipt Basic Problems/problem_5.js
@@ -0,0 +1,6 @@
+/** Problem 05 - ( PH Email Generator )  */
+var student = { name: "jhankar", roll: 1014, department: "cse" };
+
+// write your code here
+var email = student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(email);
