930763bae10a449b1decd5da217efb011cc24656
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-04 17:03:46 +0600

    Problem 04 - Delete / Store

---
+++ Assignment_3 - Javascipt Basic Problems/problem_4.js
@@ -0,0 +1,12 @@
+/** Problem 04 - (Delete / Store) */
+var fileName = "pdfData.jpg";
+//write your code here
+if (
+  fileName.startsWith("#") ||
+  fileName.endsWith(".pdf") ||
+  fileName.endsWith(".docx")
+) {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
