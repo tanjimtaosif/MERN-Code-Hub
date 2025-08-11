b6261ddd8dcaaddb07d57031319d46c5edbbc893
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-11 22:45:19 +0600

    Recap the previous learnings

---
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/recap.js
@@ -0,0 +1,15 @@
+// const : Value will not change
+// let : Value can change
+// var : we should not use this
+
+const amount = 200;
+
+if( amount>0){
+
+}
+else{
+
+}
+
+// array
+const numbers = [12, 34, 56, 10];
