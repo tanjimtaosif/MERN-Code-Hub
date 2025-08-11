df3224cadc2a71b83a3a63c80e1b52df540df27c
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-11 22:45:38 +0600

    Practiced for of loop

---
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/for-of.js
@@ -0,0 +1,17 @@
+const numbers = [12, 343, 34, 4354, 535];
+console.log(numbers); //it will print all the numbers
+
+for (const num of numbers) {
+    console.log(num);
+}
+
+const fruits = ['orange', 'apple', 'mango', 'strawberry', 'jackfruit'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits) {
+    console.log(fruit);
+
+}
