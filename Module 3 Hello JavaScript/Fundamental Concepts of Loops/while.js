d8149991785535cf8c92a9e668a5f431620a7073
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-15 03:13:35 +0600

    Simple while loop

--- Module 3 Hello JavaScript/Fundamental Concepts of Loops/while.js
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/while.js
@@ -1,8 +1,18 @@
-const num = 7;
+const number = 7;
 
-if (num < 10){
+if (number < 10) {
     console.log("It's a small number");
 }
 
 console.log('Something new is cooking');
 
+//  -----------------
+
+let num = 0;
+// while (num < 5){
+//     console.log(); // it will run infinity time
+// }
+while (num < 5) {
+    console.log("Looping", num);
+    num = num + 1;
+}
