a8b2d194611320ad707b15b5530cd3a60cc03625
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-24 04:18:35 +0600

    Basic for loop structure

---
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/for.js
@@ -0,0 +1,14 @@
+let num = 0;
+while (num < 5) {
+    console.log(num);
+    num++;
+};
+
+// for(initialization; condition; increment){
+
+// };
+
+for (let num = 0; num < 5; num++) {
+    console.log(num);
+    
+}
---
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/odd-even-for.js
@@ -0,0 +1,13 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// even
+for (i = 0; i < 20; i = i + 2) {
+    console.log("Even numbers: ", i);
+}
+
+// Odd
+for (i = 1; i < 20; i = i + 2) {
+    console.log("Odd numbers: ", i);
+}
