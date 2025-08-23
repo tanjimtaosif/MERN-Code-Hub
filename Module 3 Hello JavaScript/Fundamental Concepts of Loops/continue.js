f8ce55ea1ec222cc187bb22140730e9af5e0baab
tanjimtaosif <itsmahtab007@gmail.com>
2025-08-24 05:48:45 +0600

    Continue in js

---
+++ Module 3 Hello JavaScript/Fundamental Concepts of Loops/continue.js
@@ -0,0 +1,20 @@
+// continue --> Skip this iteration
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 2 == 1) {
+//         continue;
+//     }
+//     console.log(i);
+
+// }
+
+// while
+
+let n = 0;
+while (n < 15) {
+    n++;
+    if (n % 5 !== 0) {
+        continue;
+    }
+    console.log(n);
+}
