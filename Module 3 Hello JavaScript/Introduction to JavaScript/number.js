967ae6e68a30b31d6389d1b95bc8c8dfe9f59628
tanjimtaosif <itsmahtab007@gmail.com>
2026-05-23 01:55:46 +0600

    Fix apple variable initialization and enable console log for sum

--- Module 3 Hello JavaScript/Introduction to JavaScript/number.js
+++ Module 3 Hello JavaScript/Introduction to JavaScript/number.js
@@ -2,13 +2,13 @@ var orange = 20.4;
 var lemon = 820;
 // var apple = '5.10';
 
-var apple = parseInt('5.10');
+var apple = parseInt('5');
 var orange = parseFloat('5.10');
 // console.log(typeof orange);
 // console.log(typeof apple);
 
 
-// console.log(orange + apple);
+console.log(orange + apple);
 
 
 var first = 0.1;
